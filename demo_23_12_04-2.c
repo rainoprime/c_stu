b89629f5366229052ae608a7480852119eca114d
Jason Liu <it@liujinshui.com>
2023-12-04 20:42:03 +0800

    feat while循环

---
+++ demo_23_12_04-2.c
@@ -0,0 +1,21 @@
+//
+// Created by raino on 12/4/2023.
+//
+//while Ñ­»·
+
+#ifdef FILE_23_12_04_2_MAIN
+
+
+#include <stdio.h>
+
+
+int main() {
+    int a = 0;
+
+    while ((a = getchar()) != EOF) {
+        putchar(a);
+    }
+    return 0;
+}
+
+#endif
