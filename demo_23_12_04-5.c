d0b3e6ab32a58b7f952eda5bfccdf7fc0d6f1972
Jason Liu <it@liujinshui.com>
2023-12-04 23:00:17 +0800

    feat do while循环结构

---
+++ demo_23_12_04-5.c
@@ -0,0 +1,21 @@
+//
+// Created by raino on 12/4/2023.
+//
+// do while 循环结构
+
+#ifdef FILE_23_12_04_5_MAIN
+
+#include <stdio.h>
+
+int main() {
+    int i = 0;
+
+    do {
+        printf("开始打印数据啦 \n");
+        i++;
+    } while (i <= 5);
+
+    return 0;
+}
+
+#endif
